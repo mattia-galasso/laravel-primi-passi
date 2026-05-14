72af1a8aabf04a11a9b413cb215c2d2f87714733
MattiaGalasso <galasso.mattia94@gmail.com>
2026-05-14 17:55:56 +0200

    Update routes to include new views for home, products, contacts, and about us pages

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi Passi</title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋🏻</text></svg>">
+</head>
+
+<body>
+    <header>
+        <h1>👋🏻 Hello World! 👋🏻</h1>
+        <h2>2️⃣ - Benvenuto nella pagina Contatti</h2>
+    </header>
+    <main>
+        <h2>Altre pagine:</h2>
+        <div>
+            <ul>
+                <li><a href="{{ url('products') }}">Prodotti → 1️⃣</a></li>
+                <li><a href="{{ url('') }}">🏠 ← Torna alla Home</a></li>
+                <li><a href="{{ url('aboutus') }}">Crediti → 3️⃣</a></li>
+            </ul>
+        </div>
+    </main>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
 });
